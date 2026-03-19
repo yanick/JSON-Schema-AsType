d241efa9bdeaba0ebc971e8b54cbf466e53f4670
Yanick Champoux <yanick@babyl.ca>
2026-03-18 20:12:56 -0400

    wip

--- lib/JSON/Schema/AsType/Draft2019_09/Vocabulary/Validation.pm
+++ lib/JSON/Schema/AsType/Draft2019_09/Vocabulary/Validation.pm
@@ -26,6 +26,45 @@ with 'JSON::Schema::AsType::Draft7::Keywords' => {
 	-exclude => [ ]
 };
 
+__PACKAGE__->meta->add_method(
+	'_keyword_$recursiveRef' => sub {
+		my ( $self, $ref ) = @_;
+
+		my $schema;
+
+		return Type::Tiny->new(
+			name         => 'RecursiveRef',
+			display_name => "RecursiveRef($ref)",
+			constraint   => sub {
+
+				my $v = $_;
+
+				my $parent = $self;
+				$DB::single = 1;
+				while($parent = $parent->parent_schema ) {
+					return $parent->check($v) if $parent->schema->{'$recursiveAnchor'};
+				}
+
+				local $::DEEP = ( $::DEEP // 0 ) + 1;
+				die if $::DEEP > 10;
+				use JSON::Schema::AsType::Debug;
+				debug( 'in ref for %s', $ref );
+				$schema //= $self->resolve_reference($ref);
+
+				my $result = $schema->check($v) || 0;
+
+				return $result;
+			},
+			message => sub {
+				join "\n",
+				  "ref schema is "
+				  . to_json( $schema->schema, { allow_nonref => 1 } ),
+				  @{ $schema->validate_explain($_) };
+			}
+		);
+	}
+);
+
 sub _keyword_dependentRequired {
 	my( $self, $depends) = @_;
 
