e676fcf36501b3c42c194f0d33ac4bbccc2578cb
Yanick Champoux <yanick@cpan.org>
2026-03-21 12:51:57 -0400

    uri attribute are objects now

--- lib/JSON/Schema/AsType.pm
+++ lib/JSON/Schema/AsType.pm
@@ -25,240 +25,228 @@ use Module::Runtime qw/ use_module /;
 use Moose::Util qw/ apply_all_roles /;
 
 use JSON;
+use Type::Utils qw( class_type );
 
 use Moose;
 use MooseX::MungeHas 'is_ro';
 
 with 'JSON::Schema::AsType::Registry';
 
-
 no warnings 'uninitialized';
 
 our $strict_string = 1;
 
-our @DRAFT_VERSIONS = ( 3,4,6,7,'2019-09' );
+our @DRAFT_VERSIONS = ( 3, 4, 6, 7, '2019-09' );
 
 has type => (
-	is      => 'rwp',
-	handles => [qw/ check validate validate_explain /],
-	builder => 1,
-	lazy    => 1
+    is      => 'rwp',
+    handles => [qw/ check validate validate_explain /],
+    builder => 1,
+    lazy    => 1
 );
 
 has draft => (
-	is      => 'ro',
-	lazy    => 1,
-	default => sub {
-		  eval { $_[0]->parent_schema && $_[0]->parent_schema->draft } || $DRAFT_VERSIONS[-1];
-	},
-	isa => enum( \@DRAFT_VERSIONS ),
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        eval { $_[0]->parent_schema && $_[0]->parent_schema->draft }
+          || $DRAFT_VERSIONS[-1];
+    },
+    isa => enum( \@DRAFT_VERSIONS ),
 );
 
 has metaschema => (
-	is      => 'ro',
-	lazy    => 1,
-	default => sub {
-		$_[0]->fetch( sprintf "https://json-schema.org/draft-%02d/schema",
-			$_[0]->draft );
-	},
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        $_[0]->fetch( sprintf "https://json-schema.org/draft-%02d/schema",
+            $_[0]->draft );
+    },
 );
 
 has schema => (
-	predicate => 'has_schema',
-	is => 'ro',
-	lazy      => 1,
-	default   => sub {
-		return +{};
-		my $self = shift;
+    predicate => 'has_schema',
+    is        => 'ro',
+    lazy      => 1,
+    default   => sub {
+        return +{};
+        my $self = shift;
 
-		my $uri = $self->uri or die "schema or uri required";
+        my $uri = $self->uri or die "schema or uri required";
 
-		return $self->fetch($uri)->schema;
-	},
+        return $self->fetch($uri)->schema;
+    },
 );
 
-sub _schema_trigger {}
+sub _schema_trigger { }
 
 has parent_schema => ( clearer => 1, );
 
 has strict_string => (
-	is      => 'ro',
-	lazy    => 1,
-	default => sub {
-		my $self = shift;
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
 
-		return $self->parent_schema->strict_string if $self->parent_schema;
+        return $self->parent_schema->strict_string if $self->parent_schema;
 
-		return $JSON::Schema::AsType::strict_string;
-	},
+        return $JSON::Schema::AsType::strict_string;
+    },
 );
 
 has uri => (
-	is      => 'ro',
-	trigger => sub {
-		my ( $self, $uri ) = @_;
-		$uri = URI->new($uri);
-		return if $uri->fragment;
-		$self->clear_parent_schema;
-	}
+    is  => 'ro',
+    isa => class_type( { class => 'URI' } )->plus_constructors( Str, "new", ),
+    coerce  => 1,
+    trigger => sub {
+        my ( $self, $uri ) = @_;
+        return if $uri->fragment;
+        $self->clear_parent_schema;
+    }
 );
 
-has references => sub {
-	+{};
-};
-
 sub validate_schema {
-	my $self = shift;
-	$self->metaschema->validate( $self->schema );
+    my $self = shift;
+    $self->metaschema->validate( $self->schema );
 }
 
 sub validate_explain_schema {
-	my $self = shift;
-	$self->metaschema->validate_explain( $self->schema );
+    my $self = shift;
+    $self->metaschema->validate_explain( $self->schema );
 }
 
 sub root_schema {
-	my $self = shift;
-	eval { $self->parent_schema and $self->parent_schema->root_schema } || $self;
+    my $self = shift;
+    eval { $self->parent_schema and $self->parent_schema->root_schema }
+      || $self;
 }
 
 sub is_root_schema {
-	my $self = shift;
-	return not $self->parent_schema;
+    my $self = shift;
+    return not $self->parent_schema;
 }
 
-sub sub_schema($self,$subschema,$uri) {
+sub sub_schema( $self, $subschema, $uri ) {
 
-	$uri = $self->resolve_uri($uri) if $uri;
+    $uri = $self->resolve_uri($uri) if $uri;
 
     JSON::Schema::AsType->new(
-        draft => $self->draft,
-        schema => $subschema, 
-        parent_schema => $self, 
-        registry => $self->registry, 
-        maybe uri => $uri
+        draft         => $self->draft,
+        schema        => $subschema,
+        parent_schema => $self,
+        registry      => $self->registry,
+        maybe uri     => $uri
     );
 
 }
 
 sub _build_type {
-	my $self = shift;
+    my $self = shift;
 
-	$self->_set_type('');
+    $self->_set_type('');
 
     local %JSON::Schema::AsType::CONTEXT = ();
 
-	if( JSON::is_bool($self->schema) ) {
-		return $self->schema ? Any : ~Any;
-	}
+    if ( JSON::is_bool( $self->schema ) ) {
+        return $self->schema ? Any : ~Any;
+    }
 
-	# $ref trumps all
-	return $self->_process_keyword('$ref')
-	  if $self->schema->{'$ref'};
+    # $ref trumps all
+    return $self->_process_keyword('$ref')
+      if $self->schema->{'$ref'};
 
-	my @types =
-	  grep { $_ and $_->name ne 'Any' }
-	  map { $self->_process_keyword($_) } $self->all_keywords;
+    my @types =
+      grep { $_ and $_->name ne 'Any' }
+      map { $self->_process_keyword($_) } $self->all_keywords;
 
-	return @types ? reduce { $a & $b } @types : Any;
+    return @types ? reduce { $a & $b } @types : Any;
 }
 
 sub all_keywords {
-	my $self = shift;
+    my $self = shift;
 
-	# 'id' has to be first
-	return sort { $a eq 'id' ? -1 : $b eq 'id' ? 1 : $a cmp $b }
-	  map { /^_keyword_(.*)/ } $self->meta->get_method_list;
+    # 'id' has to be first
+    return sort { $a eq 'id' ? -1 : $b eq 'id' ? 1 : $a cmp $b }
+      map { /^_keyword_(.*)/ } $self->meta->get_method_list;
 }
 
-sub has_keyword($self,$keyword) {
+sub has_keyword( $self, $keyword ) {
     my $method = "_keyword_$keyword";
     return $self->can($method);
 }
 
 sub _process_keyword {
-	my ( $self, $keyword ) = @_;
+    my ( $self, $keyword ) = @_;
 
-	return unless exists $self->schema->{$keyword};
+    return unless exists $self->schema->{$keyword};
 
-	my $value = $self->schema->{$keyword};
+    my $value = $self->schema->{$keyword};
 
-	my $method = "_keyword_$keyword";
+    my $method = "_keyword_$keyword";
 
-	$self->$method($value);
+    $self->$method($value);
 }
 
 # returns the first defined parent uri
 sub ancestor_uri {
-	my $self = shift;
+    my $self = shift;
 
-	return $self->uri || eval { $self->parent_schema->ancestor_uri };
+    return $self->uri || eval { $self->parent_schema->ancestor_uri };
 }
 
 sub resolve_reference {
-	my ( $self, $ref ) = @_;
+    my ( $self, $ref ) = @_;
 
-	my $uri = $self->resolve_uri($ref);
+    my $uri = $self->resolve_uri($ref);
 
-	my $schema = $self->fetch($uri) or die "couldn't retrieve schema $uri\n";
+    my $schema = $self->fetch($uri) or die "couldn't retrieve schema $uri\n";
 
-	return $schema;
+    return $schema;
 }
 
 sub _unescape_ref {
-	my ( $self, $ref ) = @_;
+    my ( $self, $ref ) = @_;
 
-	$ref =~ s/~0/~/g;
-	$ref =~ s!~1!/!g;
-	$ref =~ s!%25!%!g;
-	$ref =~ s!%22!"!g;
+    $ref =~ s/~0/~/g;
+    $ref =~ s!~1!/!g;
+    $ref =~ s!%25!%!g;
+    $ref =~ s!%22!"!g;
 
-	$ref;
+    $ref;
 }
 
 sub _escape_ref {
-	my ( $self, $ref ) = @_;
+    my ( $self, $ref ) = @_;
 
-	$ref =~ s/~/~0/g;
-	$ref =~ s!/!~1!g;
-	$ref =~ s!%!%25!g;
-	$ref =~ s!"!%22!g;
+    $ref =~ s/~/~0/g;
+    $ref =~ s!/!~1!g;
+    $ref =~ s!%!%25!g;
+    $ref =~ s!"!%22!g;
 
-	$ref;
-}
-
-sub _add_reference {
-	my ( $self, $path, $schema ) = @_;
-
-	$path = join '/', '#', map { $self->_escape_ref($_) } @$path
-	  if ref $path;
-
-	$self->references->{$path} = $schema;
+    $ref;
 }
 
 sub _add_to_type {
-	my ( $self, $t ) = @_;
+    my ( $self, $t ) = @_;
 
-	if ( my $already = $self->type ) {
-		$t = $already & $t;
-	}
+    if ( my $already = $self->type ) {
+        $t = $already & $t;
+    }
 
-	$self->_set_type($t);
+    $self->_set_type($t);
 }
 
-
 sub BUILD {
-	my $self = shift;
+    my $self = shift;
 
-	use_module(
-		'JSON::Schema::AsType::Draft' . $self->draft =~ s/-/_/r
-	)->meta->rebless_instance( $self );
+    use_module( 'JSON::Schema::AsType::Draft' . $self->draft =~ s/-/_/r )
+      ->meta->rebless_instance($self);
 
-	# make it available early for the potential $refs
-	$self->register_schema( $self->uri, $self ) if $self->uri;
+    # make it available early for the potential $refs
+    $self->register_schema( $self->uri, $self ) if $self->uri;
 
-	# TODO move the role into a trait, which should take care of this
-	$self->_schema_trigger($self->schema) if $self->has_schema;
+    # TODO move the role into a trait, which should take care of this
+    $self->_schema_trigger( $self->schema ) if $self->has_schema;
 
 }
 
