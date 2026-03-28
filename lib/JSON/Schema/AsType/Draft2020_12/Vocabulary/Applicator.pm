e360269bef4bcad9731696dc090804792879ab4b
Yanick Champoux <yanick@cpan.org>
2026-03-28 15:48:10 -0400

    kinda working

--- lib/JSON/Schema/AsType/Draft2020_12/Vocabulary/Applicator.pm
+++ lib/JSON/Schema/AsType/Draft2020_12/Vocabulary/Applicator.pm
@@ -16,7 +16,7 @@ use feature qw/ module_true /;
 
 use Moose::Role;
 
-use Types::Standard                           qw/ Any ArrayRef /;
+use Types::Standard qw/ Any ArrayRef /;
 use JSON::Schema::AsType::Annotations;
 use JSON::Schema::AsType::Draft4::Types       qw/ Boolean /;
 use JSON::Schema::AsType::Draft2019_09::Types qw/ /;
@@ -29,130 +29,134 @@ with 'JSON::Schema::AsType::Draft2019_09::Vocabulary::Applicator' =>
 
 sub _keyword_prefixItems ( $self, $items, $keyword = 'prefixItems' ) {
 
-	if ( Boolean->check($items) ) {
-		return if $items;
-		return PrefixItems [JSON::false];
-	}
+    if ( Boolean->check($items) ) {
+        return if $items;
+        return PrefixItems [JSON::false];
+    }
 
-	if ( ref $items eq 'HASH' ) {
-		my $type = $self->sub_schema( $items, "#./$keyword" )->type;
+    if ( ref $items eq 'HASH' ) {
+        my $type = $self->sub_schema( $items, "#./$keyword" )->type;
 
-		return PrefixItems [$type];
-	}
+        return PrefixItems [$type];
+    }
 
-	# TODO forward declaration not workie
-	my @types;
-	my $i = 0;
-	for (@$items) {
-		push @types, $self->sub_schema( $_, "#./$keyword/" . $i++ )->type;
-	}
+    # TODO forward declaration not workie
+    my @types;
+    my $i = 0;
+    for (@$items) {
+        push @types, $self->sub_schema( $_, "#./$keyword/" . $i++ )->type;
+    }
 
-	return PrefixItems [ \@types ];
+    return PrefixItems [ \@types ];
 }
 
 sub _keyword_items {
-	my ( $self, $s ) = @_;
+    my ( $self, $s ) = @_;
 
-	my $schema = $self->sub_schema( $s, '#./items' );
+    my $schema = $self->sub_schema( $s, '#./items' );
 
-	# items is schema => additionalItems does nothing
-	return Any if ref $self->schema->{prefixItems} eq 'HASH';
+    # items is schema => additionalItems does nothing
+    return Any if ref $self->schema->{prefixItems} eq 'HASH';
 
-	my $to_skip = ( $self->schema->{prefixItems} || [] )->@*;
+    my $to_skip = ( $self->schema->{prefixItems} || [] )->@*;
 
-	return ~ArrayRef | Items [ $to_skip, $schema->type ];
+    return ~ArrayRef | Items [ $to_skip, $schema->type ];
 
 }
 
 sub _keyword_contains( $self, $schema ) {
 
-	my $type = $self->sub_schema( $schema, '#./contains' )->type;
+    my $type = $self->sub_schema( $schema, '#./contains' )->type;
 
-	my $contains = sub {
-			my $v = $_;
-			add_annotation('contains',
-			  grep { $type->check( $v->[$_] ) } 0 .. $_->$#*
-			);
-			return 1;
-		};
+    my $contains = sub {
+        my $v = $_;
+        add_annotation( 'contains',
+            grep { $type->check( $v->[$_] ) } 0 .. $_->$#* );
+        return 1;
+    };
 
-	$contains = Contains [$type] & $contains 
-		unless exists $self->schema->{minContains} 
-			and $self->schema->{minContains} == 0;
+    $contains = Contains [$type] & $contains
+      unless exists $self->schema->{minContains}
+      and $self->schema->{minContains} == 0;
 
-	return ~ArrayRef | $contains;
+    return ~ArrayRef | $contains;
 
 }
 
 __PACKAGE__->meta->add_method(
-	'_keyword_$ref' => sub {
-		my ( $self, $ref ) = @_;
+    '_keyword_$ref' => sub {
+        my ( $self, $ref ) = @_;
 
-		my $schema;
+        my $schema;
 
-		return Type::Tiny->new(
-			name         => 'Ref',
-			display_name => "Ref($ref)",
-			constraint   => sub {
-				local $::DEEP = ( $::DEEP // 0 ) + 1;
-				die if $::DEEP > 10;
-				my $v = $_;
+        return Type::Tiny->new(
+            name         => 'Ref',
+            display_name => "Ref($ref)",
+            constraint   => sub {
+                local $::DEEP = ( $::DEEP // 0 ) + 1;
+                die if $::DEEP > 10;
+                my $v = $_;
 
-				$schema = $self->resolve_reference($ref);
+                $schema = $self->resolve_reference($ref);
 
-                if( $schema ) {
-                    return $self->sub_schema( 
-                        $schema->schema, $schema->uri )->type->check($v)
+                if ($schema) {
+                    return $self->sub_schema( $schema->schema, $schema->uri )
+                      ->type->check($v);
                 }
 
-				my $m = '_keyword_$dynamicRef';
-				return $self->$m($ref);
-			},
-			message => sub {
-				join "\n",
-				  "ref schema is "
-				  . to_json( $schema->schema, { allow_nonref => 1 } ),
-				  @{ $schema->validate_explain($_) };
-			}
-		);
-	}
+                my $m = '_keyword_$dynamicRef';
+                return $self->$m($ref);
+            },
+            message => sub {
+                join "\n",
+                  "ref schema is "
+                  . to_json( $schema->schema, { allow_nonref => 1 } ),
+                  @{ $schema->validate_explain($_) };
+            }
+        );
+    }
 );
 
 __PACKAGE__->meta->add_method(
-	'_keyword_$dynamicRef' => sub {
-		my ( $self, $ref ) = @_;
+    '_keyword_$dynamicRef' => sub {
+        my ( $self, $ref ) = @_;
 
-		my $schema;
+        my $schema;
 
-		return Type::Tiny->new(
-			display_name => "DynamicRef($ref)",
-			constraint   => sub {
+        return Type::Tiny->new(
+            display_name => "DynamicRef($ref)",
+            constraint   => sub {
 
-				my $v = $_;
+                my $v = $_;
 
-				my $anchor;
-				my $parent = $self;
+                if( $ref =~ /(.+?)#(.+)/ ) {
+                    return $self->resolve_reference($ref)->base_type->check($v);
+                }
 
-				my $first_id;
+                my $anchor;
+                my $parent = $self;
 
-				$DB::single = 1;
 
-				$ref =~ s/^#//;
-				
-				$DB::single = 1;
+                my $first_id;
 
-				while ( $parent = $parent->parent_schema ) {
+                $DB::single = 1;
 
-					$anchor = $parent->_find_dynamicAnchor($ref) and last;
+                $ref =~ s/^#//;
 
-				}
+                $DB::single = 1;
 
-				die "anchor not found\n" unless $anchor;
+                while ( $parent = $parent->parent_schema ) {
 
-				# use DDP; p $anchor->schema;
-				# warn "checking for $v\n";
-				return $anchor->base_type->check($v);
-			},
-		);
-	}
+                    $anchor = $parent->_find_dynamicAnchor($ref) and last;
+
+                }
+
+                die "anchor $ref not found\n" unless $anchor;
+
+                # use DDP; p $anchor->schema;
+                # warn "checking for $v\n";
+                return $anchor->base_type->check($v);
+            },
+        );
+    }
 );
--- t/600-draft2020-12/100-dynamicRef.t
+++ t/600-draft2020-12/100-dynamicRef.t
@@ -8,52 +8,53 @@ use feature qw/ signatures/;
 use JSON::Schema::AsType;
 use JSON;
 
-subtest 'A $dynamicRef to a $dynamicAnchor in the same schema resource behaves like a normal $ref to an $anchor' => sub {
-	my $schema = JSON::Schema::AsType->new(
-		draft  => '2020-12',
-		schema => {
-			'$id'     => 'https://test.json-schema.org/dynamicRef-dynamicAnchor-same-schema/root',
-			'type'  => 'array',
-			'items' => {
-				'$dynamicRef' => '#items'
-			},
-			'$defs' => {
-				'foo' => {
-					'type'           => 'string',
-					'$dynamicAnchor' => 'items'
-				}
-			},
-		},
-	);
+subtest
+  'A $dynamicRef to a $dynamicAnchor in the same schema resource behaves like a normal $ref to an $anchor'
+  => sub {
+    my $schema = JSON::Schema::AsType->new(
+        draft  => '2020-12',
+        schema => {
+            '$id' =>
+              'https://test.json-schema.org/dynamicRef-dynamicAnchor-same-schema/root',
+            'type'  => 'array',
+            'items' => { '$dynamicRef' => '#items' },
+            '$defs' => {
+                'foo' => {
+                    'type'           => 'string',
+                    '$dynamicAnchor' => 'items'
+                }
+            },
+        },
+    );
 
-	ok $schema->check( ['foo','bar'] ), 'all good';
-	ok !$schema->check( ['foo',1] ), 'nope';
+    ok $schema->check( [ 'foo',  'bar' ] ), 'all good';
+    ok !$schema->check( [ 'foo', 1 ] ),     'nope';
 
-};
+  };
 
-subtest 'A $dynamicRef to a $anchor in the same schema resource behaves like a normal $ref to an $anchor' => sub {
-	my $schema = JSON::Schema::AsType->new(
-		draft  => '2020-12',
-		schema => {
-			'$id'     => 'https://test.json-schema.org/dynamicRef-dynamicAnchor-same-schema/root',
-			'type'  => 'array',
-			'items' => {
-				'$dynamicRef' => '#items'
-			},
-			'$defs' => {
-				'foo' => {
-					'type'    => 'string',
-					'$anchor' => 'items'
-				}
-			},
-		},
-	);
+subtest
+  'A $dynamicRef to a $anchor in the same schema resource behaves like a normal $ref to an $anchor'
+  => sub {
+    my $schema = JSON::Schema::AsType->new(
+        draft  => '2020-12',
+        schema => {
+            '$id' =>
+              'https://test.json-schema.org/dynamicRef-dynamicAnchor-same-schema/root',
+            'type'  => 'array',
+            'items' => { '$dynamicRef' => '#items' },
+            '$defs' => {
+                'foo' => {
+                    'type'    => 'string',
+                    '$anchor' => 'items'
+                }
+            },
+        },
+    );
 
-	ok $schema->check( ['foo','bar'] ), 'all good';
-	ok !$schema->check( ['foo',1] ), 'nope';
+    ok $schema->check( [ 'foo',  'bar' ] ), 'all good';
+    ok !$schema->check( [ 'foo', 1 ] ),     'nope';
 
-};
-
+  };
 
 subtest 'A $ref to a $dynamicAnchor' => sub {
     my $schema = JSON::Schema::AsType->new(
@@ -70,15 +71,84 @@ subtest 'A $ref to a $dynamicAnchor' => sub {
         },
     );
 
-    use DDP;
-    p $schema->resolve_reference('#items')->type->check(1);
-    p [$schema->all_schema_uris]->@*;
-
-
-
     ok $schema->check( [ 'foo',  'bar' ] ), 'all good';
-    ok !$schema->check( [ 'foo', [] ] ),     'nope';
+    ok !$schema->check( [ 'foo', [] ] ),    'nope';
+};
 
+subtest 'recursive part is valid against the root' => sub {
+    my $schema = JSON::Schema::AsType->new(
+        draft  => '2020-12',
+        schema => {
+            '$dynamicAnchor' => 'meta',
+            '$schema' => 'https://json-schema.org/draft/2020-12/schema',
+            '$id'     =>
+              'https://test.json-schema.org/relative-dynamic-reference/root',
+            'type'       => 'object',
+            'properties' => { 'foo' => { 'const' => 'pass' } },
+            '$ref'       => 'extended',
+            '$defs'      => {
+                'extended' => {
+                    'properties'     => { 'bar' => { '$ref' => 'bar' } },
+                    '$dynamicAnchor' => 'meta',
+                    '$id'            =>
+                      'https://test.json-schema.org/relative-dynamic-reference/extended',
+                    'type' => 'object'
+                },
+                'bar' => {
+                    '$id' =>
+                      'https://test.json-schema.org/relative-dynamic-reference/bar',
+                    'type'       => 'object',
+                    'properties' =>
+                      { 'baz' => { '$dynamicRef' => 'extended#meta' } }
+                }
+            },
+        },
+    );
+
+    ok $schema->check(
+        {   'foo' => 'pass',
+            'bar' => { 'baz' => { 'foo' => 'pass' } }
+        }
+      ),
+      'all good';
+};
+
+subtest 'The recursive part is valid against the root' => sub {
+    my $schema = JSON::Schema::AsType->new(
+        draft  => '2020-12',
+        schema => {
+            '$id' =>
+              'https://test.json-schema.org/relative-dynamic-reference/root',
+            '$schema' => 'https://json-schema.org/draft/2020-12/schema',
+            '$ref'    => 'extended',
+            '$dynamicAnchor' => 'meta',
+            '$defs'          => {
+                'bar' => {
+                    'properties' =>
+                      { 'baz' => { '$dynamicRef' => 'extended#meta' } },
+                    'type' => 'object',
+                    '$id'  =>
+                      'https://test.json-schema.org/relative-dynamic-reference/bar'
+                },
+                'extended' => {
+                    'type'           => 'object',
+                    'properties'     => { 'bar' => { '$ref' => 'bar' } },
+                    '$dynamicAnchor' => 'meta',
+                    '$id'            =>
+                      'https://test.json-schema.org/relative-dynamic-reference/extended'
+                }
+            },
+            'type'       => 'object',
+            'properties' => { 'foo' => { 'const' => 'pass' } }
+        }
+    );
+
+    ok $schema->check(
+        {   'foo' => 'pass',
+            'bar' => { 'baz' => { 'foo' => 'pass' } }
+        }
+      ),
+      'all good';
 };
 
 done_testing;
